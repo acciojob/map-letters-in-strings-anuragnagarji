8eacca45fbb61cc543c343dd9d319bc6df7602a8
anuragnagarji <142933378+anuragnagarji@users.noreply.github.com>
2023-12-30 12:37:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 //your JS code here. If required.
-function mapletters(str) {
-	let tempObj={};
-	str=str.toLowerCase();
-	for(let i=0;i<str.length;i++){
-	if(tempObj[str[i]]){
-		tempObj[str[i]].push(i);
-	}
-	else{
-		tempObj[str[i]]=[i];
-	}
-	}
-return tempObj;
+function mapLetters(word) {
+  const letterMap = {};
+ 
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    
+    if (!letterMap[letter]) {
+      // If the letter is not in the map, create an array for it
+      letterMap[letter] = [i];
+    } else {
+      // If the letter is already in the map, push the index to its array
+      letterMap[letter].push(i);
+    }
+  }
+ 
+  return letterMap;
 }
-let str=prompt();
-
-alert(mapletters(str));
+ 
+// Examples
+console.log(mapLetters("dodo"));   // Output: { d: [0, 2], o: [1, 3] }
+console.log(mapLetters("froggy")); // Output: { f: [0], r: [1], o: [2], g: [3, 4], y: [5] }
+console.log(mapLetters("grapes")); // Output: { g: [0], r: [1], a: [2], p: [3], e: [4], s: [5] }
